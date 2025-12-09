dd7e7a168efafa979afcc4489425c6825b7e0e0f
Mick Doldersum <mickdoldersum@gmail.com>
2025-12-10 00:24:57 +0100

    fix GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,17 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <head>
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <Script id="google-analytics">
-            {`<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-1WH36J7F55"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-1WH36J7F55');
-</script>`}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-1WH36J7F55"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1WH36J7F55');
+            `}
           </Script>
         </head>
         <body suppressHydrationWarning>
